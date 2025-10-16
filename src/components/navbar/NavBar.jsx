96a7ca2ac7aee1c205cb639a8b8520c705651139
UalkerConde <ualker_jf@yahoo.com.br>
2025-10-15 21:20:46 -0300

    feat: Criada NavBar

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return(
+        <nav>
+            <h1>ToDo</h1>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/Tarefas">Tarefas</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import Login from './pages/Login/Login.jsx'
 import "./global.css"
 import Cadastro from './pages/Cadastro/Cadastro.jsx'
+import Navbar from './components/navbar/NavBar.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <header>
+      <Navbar/>
+    </header>
+    <main></main>
+    <footer></footer>
+    
     {/* <Login /> */}
-    <Cadastro />
+    {/* <Cadastro /> */}
   </StrictMode>,
 )
