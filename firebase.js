5229ee5b12020f195e998fa1fdf01e3f3a04eb64
UalkerConde <ualker_jf@yahoo.com.br>
2025-10-22 22:05:02 -0300

    feat: criando firebase

---
+++ firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+if(
+    !firebaseConfig.apiKey ||
+    !firebaseConfig.authDomain ||
+    !firebaseConfig.projectId ||
+    !firebaseConfig.storageBucket ||
+    !firebaseConfig.messagingSenderId ||
+    !firebaseConfig.appId
+){
+    console.warn("Firebase: Uma ou mais variáveis de ambiente não estão definidas corretamente.");
+}
+
+if(process.env.NODE_ENV !== 'development'){
+    console.log("Firebase Config:", firebaseConfig );
+}
+
+export const app=initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,15 +1,39 @@
 import "./Cadastro.css"
+import { db } from "../../firebase.js"
+import { collection, addDoc } from "firebase/firestore";
 
-export default function Cadastro(){
-    return(
+export default function Cadastro() {
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            alert("As senhas não coincidem!");
+            return;
+        } else {
+            addDoc(collection(db, "user"), {
+                nome: e.target.nome.value,
+                email: e.target.email.value,
+                password: password
+            })
+                .then(() => {
+                    alert("Usuário cadastrado com sucesso!");
+                })
+                .catch((error) => {
+                    console.error("Erro ao cadastrar usuário: ", error);
+                });
+        }
+    }
+    return (
         <div className="cadastro-container">
             <form id="cadastro-card" action="">
                 <h2>Cadastro</h2>
-                <input type="text" placeholder="Usuário"/>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Senha..."/>
-                <input type="password" placeholder="Confirme a senha..."/>
-                <button type="submit">Cadastrar</button>
+                <input name="nome" type="text" placeholder="Usuário" required/>
+                <input name="email" type="email" placeholder="Email" required/>
+                <input name="password" type="password" placeholder="Senha..." required value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                <input name="confirmPassword" type="password" placeholder="Confirme a senha..." required/>
+                <button type="submit" onClick={handleSubmit}>Cadastrar</button>
             </form>
         </div>
     )
